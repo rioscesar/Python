411661985669661eb340710354cd0b85db6c3614
rioscesar <rios_cesar@utexas.edu>
2015-03-29 12:15:01 -0500

    MainWindowLayout added. The difference between a Qwidget and Qmainwindow is when you need toolbars and menus

---
+++ MathGame/MainWindowLayout.py
@@ -0,0 +1,64 @@
+import sys
+from PySide import QtGui, QtCore
+
+
+class Example(QtGui.QMainWindow): # this should be a QWidget since I'm not actually doing an application but a widget...
+    def __init__(self):
+        super(Example, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        btn = QtGui.QPushButton('Quit', self)
+        btn.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn.setToolTip('This is a <b>QPushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 25)
+
+        self.statusBar().showMessage('Kill everybody')
+
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exitAction = QtGui.QAction(QtGui.QIcon('web.jpg'), 'Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(self.close)
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+        fileMenu.addAction(exitAction)
+
+        self.toolbar = self.addToolBar('Exit')
+        self.toolbar.addAction(exitAction)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Menubar')
+        self.show()
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message', "Are you sure to quit?", QtGui.QMessageBox.Yes |
+                                           QtGui.QMessageBox.No, QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            event.accept()
+        else:
+            event.ignore()
+
+    def center(self):
+        qr = self.frameGeometry()
+        cp = QtGui.QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
