63e74db06781f67164815d926e2562e1c88a5ccf
rioscesar <rios_cesar@utexas.edu>
2015-03-29 12:43:47 -0500

    Know enough to start the Math Game!

--- MathGame/Calculator Skeleton.py
+++ MathGame/Calculator Skeleton.py
@@ -46,3 +46,48 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+'''
+THIS IS THE CODE TO DO CERTAIN ACTIONS
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        btn1 = QtGui.QPushButton("Button 1", self)
+        btn1.move(30, 50)
+
+        btn2 = QtGui.QPushButton("Button 2", self)
+        btn2.move(150, 50)
+
+        btn1.clicked.connect(self.buttonClicked)
+        btn2.clicked.connect(self.buttonClicked)
+
+        self.statusBar()
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Event sender')
+        self.show()
+
+    def buttonClicked(self):
+
+        sender = self.sender()
+        self.statusBar().showMessage(sender.text() + ' was pressed')
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+'''
---
+++ MathGame/Test.py
@@ -0,0 +1,132 @@
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        col = QtGui.QColor(0, 0, 0)
+
+        self.btn = QtGui.QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+
+        self.btn.clicked.connect(self.showDialog)
+
+        self.frm = QtGui.QFrame(self)
+        self.frm.setStyleSheet("QWidget { background-color: %s }"
+            % col.name())
+        self.frm.setGeometry(130, 22, 100, 100)
+
+        self.setGeometry(300, 300, 250, 180)
+        self.setWindowTitle('Color dialog')
+        self.show()
+
+    def showDialog(self):
+
+        col = QtGui.QColorDialog.getColor()
+
+        if col.isValid():
+            self.frm.setStyleSheet("QWidget { background-color: %s }"
+                % col.name())
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+
+''' THIS IS AN EXAMPLE OF A POP OUT WINDOW THAT ASKS FOR INPUT
+import sys
+from PySide import QtGui
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        self.btn = QtGui.QPushButton('Dialog', self)
+        self.btn.move(20, 20)
+        self.btn.clicked.connect(self.showDialog)
+
+        self.le = QtGui.QLineEdit(self)
+        self.le.move(130, 22)
+
+        self.setGeometry(300, 300, 290, 150)
+        self.setWindowTitle('Input dialog')
+        self.show()
+
+    def showDialog(self):
+        text, ok = QtGui.QInputDialog.getText(self, 'Input Dialog',
+            'Enter your name:')
+
+        if ok:
+            self.le.setText(str(text))
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+'''
+
+
+
+
+
+
+
+
+''' THIS IS A NEAT LITTLE LCD NUMBER CHANGER
+import sys
+from PySide import QtGui, QtCore
+
+class Example(QtGui.QWidget):
+
+    def __init__(self):
+        super(Example, self).__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        lcd = QtGui.QLCDNumber(self)
+        sld = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+
+        vbox = QtGui.QVBoxLayout()
+        vbox.addWidget(lcd)
+        vbox.addWidget(sld)
+
+        self.setLayout(vbox)
+        sld.valueChanged.connect(lcd.display)
+
+        self.setGeometry(300, 300, 250, 150)
+        self.setWindowTitle('Signal & slot')
+        self.show()
+
+def main():
+
+    app = QtGui.QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
+'''
